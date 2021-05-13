a375a4840c8a6a835bae5522641509b62afc1c7a
amritapuranik1802 <amrita.puranik@digitalonus.com>
2021-05-13 19:45:02 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,7 +1,7 @@
 
 module "s3-bucket" {
   source  = "app.terraform.io/amrita-training/s3-bucket/aws"
-  version = "1.15.0"
+  version = "2.1.0"
   bucket_prefix = "ap"
   # insert required variables here
 }
